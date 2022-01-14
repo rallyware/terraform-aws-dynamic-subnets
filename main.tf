0ce5f411244ade3fe0534de9983c245ad7f8274f
Vladimir <26582191+SweetOps@users.noreply.github.com>
2022-01-14 19:43:00 +0200

    feat: initial implementation (#1)

--- main.tf
+++ main.tf
@@ -1,3 +1,48 @@
+# Get object aws_vpc by vpc_id
+data "aws_vpc" "default" {
+  count = local.enabled ? 1 : 0
+  id    = var.vpc_id
+}
+
+data "aws_availability_zones" "available" {
+  count = local.enabled_count
+}
+
+data "aws_caller_identity" "default" {
+  count = local.nat_instance_enabled ? 1 : 0
+}
+
+data "aws_region" "default" {
+  count = local.nat_instance_enabled ? 1 : 0
+}
+
+data "aws_partition" "default" {
+  count = local.nat_instance_enabled ? 1 : 0
+}
+
 locals {
-  enabled = module.this.enabled
+  availability_zones_count = local.enabled ? length(var.availability_zones) : 0
+  enabled                  = module.this.enabled
+  enabled_count            = local.enabled ? 1 : 0
+  delimiter                = module.this.delimiter
+}
+
+data "aws_eip" "nat_ips" {
+  count     = local.enabled ? length(var.nat_elastic_ips) : 0
+  public_ip = element(var.nat_elastic_ips, count.index)
+}
+
+locals {
+  use_existing_eips = length(var.nat_elastic_ips) > 0
+  map_map = {
+    short = "to_short"
+    fixed = "to_fixed"
+    full  = "identity"
+  }
+  az_map = module.utils.region_az_alt_code_maps[local.map_map[var.availability_zone_attribute_style]]
+}
+
+module "utils" {
+  source  = "cloudposse/utils/aws"
+  version = "0.8.1"
 }
---
+++ variables.tf
@@ -0,0 +1,185 @@
+variable "subnet_type_tag_key" {
+  type        = string
+  default     = "cpco.io/subnet/type"
+  description = "Key for subnet type tag to provide information about the type of subnets, e.g. `cpco.io/subnet/type=private` or `cpco.io/subnet/type=public`"
+}
+
+variable "subnet_type_tag_value_format" {
+  default     = "%s"
+  description = "This is using the format interpolation symbols to allow the value of the subnet_type_tag_key to be modified."
+  type        = string
+}
+
+variable "max_subnet_count" {
+  default     = 0
+  description = "Sets the maximum amount of subnets to deploy. 0 will deploy a subnet for every provided availablility zone (in `availability_zones` variable) within the region"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID where subnets will be created (e.g. `vpc-aceb2723`)"
+}
+
+variable "igw_id" {
+  type        = string
+  description = "Internet Gateway ID the public route table will point to (e.g. `igw-9c26a123`)"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "Base CIDR block which will be divided into subnet CIDR blocks (e.g. `10.0.0.0/16`)"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of Availability Zones where subnets will be created"
+
+  validation {
+    condition     = length(var.availability_zones) > 0
+    error_message = "Availability zones must be greater than zero."
+  }
+}
+
+variable "availability_zone_attribute_style" {
+  type        = string
+  default     = "short"
+  description = "The style of Availability Zone code to use in tags and names. One of `full`, `short`, or `fixed`."
+}
+
+variable "vpc_default_route_table_id" {
+  type        = string
+  default     = ""
+  description = "Default route table for public subnets. If not set, will be created. (e.g. `rtb-f4f0ce12`)"
+}
+
+variable "public_network_acl_id" {
+  type        = string
+  default     = ""
+  description = "Network ACL ID that will be added to public subnets. If empty, a new ACL will be created"
+}
+
+variable "private_network_acl_id" {
+  type        = string
+  description = "Network ACL ID that will be added to private subnets. If empty, a new ACL will be created"
+  default     = ""
+}
+
+variable "nat_gateway_enabled" {
+  type        = bool
+  description = "Flag to enable/disable NAT Gateways to allow servers in the private subnets to access the Internet"
+  default     = true
+}
+
+variable "nat_instance_enabled" {
+  type        = bool
+  description = "Flag to enable/disable NAT Instances to allow servers in the private subnets to access the Internet"
+  default     = false
+}
+
+variable "nat_instance_type" {
+  type        = string
+  description = "NAT Instance type"
+  default     = "t4g.micro"
+}
+
+variable "nat_instance_ami_architecture" {
+  type        = string
+  description = "NAT Instance AMI architecture"
+  default     = "arm64"
+}
+
+variable "nat_instance_ami_lock" {
+  type        = bool
+  description = "Whether to lock NAT Instance AMI vesrion"
+  default     = true
+}
+
+variable "nat_instance_ssh_key_pair" {
+  type        = string
+  description = "SSH key pair to be provisioned on the NAT Instance"
+  default     = null
+}
+
+variable "nat_elastic_ips" {
+  type        = list(string)
+  default     = []
+  description = "Existing Elastic IPs to attach to the NAT Gateway(s) or Instance(s) instead of creating new ones."
+}
+
+variable "nat_instance_cloudwatch_metric_alarm" {
+  type = object({
+    comparison_operator = string
+    evaluation_periods  = number
+    metric_name         = string
+    namespace           = string
+    period              = number
+    statistic           = string
+    threshold           = number
+    action              = string
+  })
+  default = {
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods  = 5
+    metric_name         = "StatusCheckFailed_Instance"
+    namespace           = "AWS/EC2"
+    period              = 60
+    statistic           = "Maximum"
+    threshold           = 1
+    action              = "action/actions/AWS_EC2.InstanceId.Reboot/1.0"
+  }
+  description = "NAT Instance CloudWatch Metric Alarm resource config"
+}
+
+variable "map_public_ip_on_launch" {
+  type        = bool
+  default     = true
+  description = "Instances launched into a public subnet should be assigned a public IP address"
+}
+
+variable "aws_route_create_timeout" {
+  type        = string
+  default     = "2m"
+  description = "Time to wait for AWS route creation specifed as a Go Duration, e.g. `2m`"
+}
+
+variable "aws_route_delete_timeout" {
+  type        = string
+  default     = "5m"
+  description = "Time to wait for AWS route deletion specifed as a Go Duration, e.g. `5m`"
+}
+
+variable "private_subnets_additional_tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags to be added to private subnets"
+}
+
+variable "public_subnets_additional_tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags to be added to public subnets"
+}
+
+variable "metadata_http_endpoint_enabled" {
+  type        = bool
+  default     = true
+  description = "Whether the metadata service is available"
+}
+
+variable "metadata_http_put_response_hop_limit" {
+  type        = number
+  default     = 1
+  description = "The desired HTTP PUT response hop limit (between 1 and 64) for instance metadata requests."
+}
+
+variable "metadata_http_tokens_required" {
+  type        = bool
+  default     = true
+  description = "Whether or not the metadata service requires session tokens, also referred to as Instance Metadata Service Version 2."
+}
+
+variable "root_block_device_encrypted" {
+  type        = bool
+  default     = true
+  description = "Whether to encrypt the root block device"
+}
